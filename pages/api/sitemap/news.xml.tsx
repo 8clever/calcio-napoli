dcfd64010e5c62768d4840271764f68b82c2ea27
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2021-06-26 16:19:35 +0300

    change sitemap

---
+++ pages/api/sitemap/news.xml.tsx
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { media } from "../../../src/components/Media";
+import { item, container } from "./static.xml";
+import { Client } from "youtubei";
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+  const { pages = "0" } = req.query;
+  const items: string[] = [];
+
+  const yt = new Client();
+  const ch = await yt.findOne(media.channelName, { type: "channel" });
+  await ch?.nextVideos(Number(pages));
+
+  ch?.videos.forEach(i => {
+    items.push(item("/news/" + i.id, "weekly"));
+  });
+  
+  res.setHeader("Content-type", "application/xml; charset=utf-8");
+  res.send(
+    container(
+      items.join("")
+    )
+  );
+}
---
+++ pages/api/sitemap/static.xml.tsx
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { media } from "../../../src/components/Media";
+
+export const container = (body: string) => {
+  return (
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       ${body}
+    </urlset> 
+    `
+  )
+}
+
+export const item = (path: string = "", changefreq: "hourly" | "weekly" | "daily" | "never") => {
+  return (
+    `
+    <url>
+      <loc>${media.domain + path}</loc>
+      <changefreq>${changefreq}</changefreq>
+    </url>
+    `
+  )
+}
+
+export default async function handler (
+  req: NextApiRequest, 
+  res: NextApiResponse
+) {
+  req
+  const items: string[] = [];
+
+  [ media.menu ].forEach(m => {
+    m.forEach(i => {
+      items.push(item(i.href, i.changefreq || "weekly"))
+    })
+  });
+
+  res.setHeader("Content-type", "application/xml; charset=utf-8");
+  res.send(
+    container(
+      items.join("")
+    )
+  );
+}
