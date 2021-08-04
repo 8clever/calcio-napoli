4e06505de2494b3f274454632c853d96173d7a47
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2021-08-04 19:33:40 +0200

    add docker-compose.yml

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
               )
             })}
           </Row>
-          <small>v1.0.2</small>
+          <small>v1.0.3</small>
         </Container>
       </footer>
       <style jsx>{`
