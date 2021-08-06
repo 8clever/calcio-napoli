29dbed171a153ff5f23463d633ea22c1f96e4e0d
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2021-08-06 18:40:01 +0200

    add small fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
               )
             })}
           </Row>
-          <small>v1.0.4</small>
+          <small>v1.0.5</small>
         </Container>
       </footer>
       <style jsx>{`
