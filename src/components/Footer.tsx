0acfbbbc783f5151e0c39d10ee13db61097f51a0
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2021-08-07 13:41:48 +0200

    change docker-compose

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
               )
             })}
           </Row>
-          <small>v1.0.6</small>
+          <small>v1.0.7</small>
         </Container>
       </footer>
       <style jsx>{`
