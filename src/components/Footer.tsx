820f21ed95a5d71650c09f8da0ceeb1fcdd4ef8a
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2021-08-10 12:44:52 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
               )
             })}
           </Row>
-          <small>v1.0.7</small>
+          <small>v1.0.8</small>
         </Container>
       </footer>
       <style jsx>{`
