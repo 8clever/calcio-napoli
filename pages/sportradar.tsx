b22c5ee5648db48e88ec0f0fca11a4312701dc0d
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2021-08-13 23:45:05 +0200

    add small fix

--- pages/sportradar.tsx
+++ pages/sportradar.tsx
@@ -1,8 +1,6 @@
 import { Container } from "../src/components/Grid";
 import Layout from "../src/components/Layout"
 import React from "react";
-import { ResponsiveIframe } from "../src/components/ResponsiveIframe";
-import moment from "moment-timezone";
 
 const title = "Calcio Napoli - Statistiche dettagliate della partita";
 
@@ -10,18 +8,13 @@ export const SportItalia = () => {
   return (
     <Layout title={title} description={title}>
       <Container page fluid>
-        <ResponsiveIframe 
-          src="/sportitalia" 
-          // taggedElement=".sr-sc-content"
-          onComplete={(cw: any) => {
-            /**
-             * seems sportitalia not working and not maintained
-             */
-            const timezone = moment.tz.guess();
-            cw.scAppOptions.timezone = timezone;
-            cw.scAppOptions.cctx.timezone = timezone;
-            cw.sportcenterInit();
+        <iframe
+          style={{
+            height: "200vh",
+            width: "100%"
           }}
+          frameBorder="false"
+          src='https://sportcenter.sir.sportradar.com/sportitalia/' 
         />
       </Container>
     </Layout>
